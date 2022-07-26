69382e5150e5bbefec2e11e663a9a099e15bcb66
Brian Barbieri <bb@letsgrow.com>
2022-07-26 16:42:26 +0200

    Made it more mobile friendly

--- response.js
+++ response.js
@@ -1,4 +1,14 @@
- 
+// Script to open and close sidebar when on tablets and phones
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+   
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
+   
 // Slideshow Images
 var slideIndex = 1;
 showDivs(slideIndex, "1");
